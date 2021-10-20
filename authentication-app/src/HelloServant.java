9f441de6a6f80179259584dc7d354117eded5720
Shuhab Siddigi <ss@pop-os.localdomain>
2021-10-20 11:10:16 +0200

    Simple client server example working

---
+++ authentication-app/src/HelloServant.java
@@ -0,0 +1,18 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class HelloServant extends UnicastRemoteObject implements HelloService{
+
+
+  public HelloServant() throws RemoteException{
+    super();
+  }
+  
+  public String echo(String input) throws RemoteException{
+  return "Hello from Server" + input;
+  }
+
+
+
+  
+}
