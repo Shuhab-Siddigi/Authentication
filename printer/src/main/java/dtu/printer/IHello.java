8e252b8bb142bad6f402f2ab12bb1187d72bd17f
Shuhab Siddigi <ss@pop-os.localdomain>
2021-10-21 11:32:21 +0200

    first test works

---
+++ printer/src/main/java/dtu/printer/IHello.java
@@ -0,0 +1,10 @@
+package dtu.printer;
+// Step 1 
+// Creating a IHello interface
+
+import java.rmi.*;
+public interface IHello extends Remote
+{
+    // Declaring the method prototype
+    public String query(String greeting) throws RemoteException;
+}
---
+++ printer/src/main/java/dtu/printer/Server.java
@@ -0,0 +1,37 @@
+package dtu.printer;
+// Step 3
+// Use Interface and class
+// Create rmiregistry
+// Bind class interface obj to endpoint
+
+import java.rmi.*;
+import java.rmi.registry.*;
+
+public class Server {
+    public static void main(String args[]) {
+
+        String IP = "localhost:";
+        String port = "1900";
+        String endpoint = "/greeting";
+        String connectionString = "rmi://" + IP + port + endpoint;
+
+        try {
+            // Create an object of the interface
+            // implementation class
+            // OPS
+            // (Iterface = new Class )
+            Hello hello = new Hello();
+
+            // rmiregistry within the server JVM with
+            // port number 1900
+            LocateRegistry.createRegistry(1900);
+
+            // Binds the remote object by the name
+            // geeksforgeeks
+            Naming.rebind(connectionString, hello);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
